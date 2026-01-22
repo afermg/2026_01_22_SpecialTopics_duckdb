8fd432a15d3e6d5553ffaf90bca99e721b4b126f
Alán F. Muñoz <afer.mg@gmail.com>
2026-01-22 00:09:26 -0500

    feat(org): Configure LaTeX export and beamerposter
    Set xelatex as the default compiler and configure latexmk with
    shell-escape support. Enable minted for source code highlighting and
    add a custom beamerposter class definition.

--- config.el
+++ config.el
@@ -7,3 +7,36 @@
   (org-babel-do-load-languages
    'org-babel-load-languages
    (append org-babel-load-languages '((duckdb . t)))))
+
+(setq org-latex-compiler "xelatex")
+(setq latex-run-command "xelatex")
+(setq org-latex-pdf-process
+      '("latexmk -pdflatex='xelatex -shell-escape -interaction nonstopmode ' -shell-escape -pdf -f %f "
+        "latexmk -pdflatex='xelatex -interaction -shell-escape nonstopmode ' -shell-escape -pdf -f %f "
+        "latexmk -pdflatex='xelatex -shell-escape -interaction nonstopmode ' -shell-escape -pdf -f %f "))
+
+(setq org-latex-prefer-user-labels t
+      org-latex-caption-above nil
+      ;; org-latex-listings 'minted
+      org-latex-listings nil
+      )
+
+
+;;Colours
+(add-to-list 'org-latex-packages-alist '("" "minted" nil))
+(setq org-latex-minted-options nil)
+(setq org-latex-src-block-backend 'minted)
+
+(add-to-list 'org-latex-classes
+             '("beamerposter"
+               "\\documentclass[final]{beamer}
+            \\usepackage[T1]{fontenc}
+            \\usepackage{lmodern}
+            \\usepackage[size=custom,width=84.1,height=118.9,scale=1.0]{beamerposter}  
+            \\usepackage{graphicx}
+            \\usepackage{booktabs}
+            \\usepackage{tikz}
+            \\usepackage{pgfplots}
+            \\pgfplotsset{compat=1.18}
+            \\usepackage{anyfontsize}
+            [NO-DEFAULT-PACKAGES]"))
